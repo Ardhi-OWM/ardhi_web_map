25c213a5347f83affbe1e26cfa27b93cfc27ee90
Ochwada <nakhulolinda@yahoo.com>
2024-09-25 12:55:42 +0200

    reconfig for the tailwind css to work

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+
+const App = () => {
+  return (
+    <>
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+        <p className="text-3xl font-bold"> Hallo World! </p>
+      </div>
+
+
+    </>
+  )
+};
+
+export default App;
